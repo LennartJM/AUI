e4e9a3463a04bd5cd4cb072c16a88657ade4be24
Ruben <ruben.oosterink@gmail.com>
2019-06-02 19:56:46 +0200

    tafel toekennen toevoegen

--- src/AUI-app/header-app.js
+++ src/AUI-app/header-app.js
@@ -63,7 +63,8 @@ class HeaderApp extends PolymerElement {
           {naam: "Terug naar home screen", link: "/"},
           {naam: "Bestelling invoeren", link: "/bestel.html"},
           {naam: "Gerecht status", link: "/status-gerecht.html"},
-          {naam: "Tafel toevoegen", link: "/tafeltoekennen.html"}
+          {naam: "Tafel toevoegen", link: "/tafel-toevoegen.html"},
+          {naam: "Tafel toekennen", link: "/tafel-toekennen.html"}
 
         ]
       }
